3adca0d381e97565495e73ad1699f4993a9f6786
OverKnigth <stalin.m0012@gmail.com>
2025-03-28 01:43:09 -0500

    new

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,34 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'app-log1',
+        appId: '1:868728431563:web:c80e5601464db00eedcc4e',
+        databaseURL: 'https://app-log1-default-rtdb.firebaseio.com',
+        storageBucket: 'app-log1.firebasestorage.app',
+        apiKey: 'AIzaSyDXev9YL6wRkdXVezshxFxXiBRwXFm37BE',
+        authDomain: 'app-log1.firebaseapp.com',
+        messagingSenderId: '868728431563',
+        measurementId: 'G-W8NK9S8YNJ',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home', component: HomeComponent},
+    {path:'nosotros', component: NosotrosComponent},
+    {path:"login", component: LoginComponent},
+    {path:'', redirectTo:'home', pathMatch: 'full'},
+    {path:'**', component: NotFoundComponent}
+];
