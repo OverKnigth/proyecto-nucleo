f657a8c04e73047a311e890b9ef94b9541f0ae93
OverKnigth <stalin.m0012@gmail.com>
2025-03-30 17:40:20 -0500

    ward

--- src/app/guards/warnigs/warning.guard.ts
+++ src/app/guards/warnigs/warning.guard.ts
@@ -1,5 +1,11 @@
 import { CanDeactivateFn } from '@angular/router';
+import { InscribirseComponent } from '../../pages/inscribirse/inscribirse.component';
 
 export const warningGuard: CanDeactivateFn<unknown> = (component, currentRoute, currentState, nextState) => {
+  const currentComponent = component as InscribirseComponent;
+
+  if(currentComponent.inscripcionForm.invalid && currentComponent.inscripcionForm.dirty) {
+    return window.confirm('Desea salir sin guardar?');
+  }
   return true;
 };
